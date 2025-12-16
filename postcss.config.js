fea75683e873e6db43bc33bc96bd954405bff104
JavCan <javicanella69@gmai.com>
2025-12-16 02:55:30 -0600

    visble

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+// Este archivo ayuda a tu editor de código a reconocer las directivas @tailwind.
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
